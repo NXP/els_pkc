214438729f199eb425f83ac0b0229ad08953e2c0
Waqar Ali Tahir <waqar.tahir@nxp.com>
2023-05-31 10:52:11 +0200

    [WCBRD-1336] fixed max charterer length file names in docs folder of clib.

--- doc/redfinchSdk_sample/html/search/groups_7.js
+++ doc/redfinchSdk_sample/html/search/groups_7.js
@@ -17,7 +17,7 @@ var searchData=
   ['mcuxclecc_5feddsa_5fed25519_5fsize_5f',['MCUXCLECC_EDDSA_ED25519_SIZE_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___e_d25519___s_i_z_e__.html',1,'']]],
   ['mcuxclecc_5feddsa_5fed448_5fsize_5f',['MCUXCLECC_EDDSA_ED448_SIZE_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___e_d448___s_i_z_e__.html',1,'']]],
   ['mcuxclecc_5feddsa_5fgenkeypair_5fdesc_5fsize_5f',['MCUXCLECC_EDDSA_GENKEYPAIR_DESC_SIZE_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___g_e_n_k_e_y_p_a_i_r___d_e_s_c___s_i_z_e__.html',1,'']]],
-  ['mcuxclecc_5feddsa_5fsignature_5fprotocol_5fdesc_5fsize_5f',['MCUXCLECC_EDDSA_SIGNATURE_PROTOCOL_DESC_SIZE_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___s_i_g_n_a_t_u_r_e___p_r_o_t_o_c_o_l___d_e_s_c___s_i_z_e__.html',1,'']]],
+  ['mcuxclecc_5feddsa_5fsignature_5fprotocol_5fdesc_5fsize_5f',['MCUXCLECC_EDDSA_SIGNATURE_PROTOCOL_DESC_SIZE_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___s_i_g_n_a___p_r_o_t_o_c_o_l___d_e_s_c___s_i_z_e__.html',1,'']]],
   ['mcuxclecc_5feddsa_5fwacpu_5f',['MCUXCLECC_EDDSA_WACPU_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___w_a_c_p_u__.html',1,'']]],
   ['mcuxclecc_5feddsa_5fwapkc_5f',['MCUXCLECC_EDDSA_WAPKC_',['../group___m_c_u_x_c_l_e_c_c___e_d_d_s_a___w_a_p_k_c__.html',1,'']]],
   ['mcuxclecc_5ffunctions',['mcuxClEcc_Functions',['../group__mcux_cl_ecc___functions.html',1,'']]],
