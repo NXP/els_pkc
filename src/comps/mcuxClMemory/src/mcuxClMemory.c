1ce37cbeebb3fc126502f887985a00b9099f91f9
Waqar Ali Tahir <waqar.tahir@nxp.com>
2023-08-11 13:28:30 +0200

    [WCBRD-1375] Updated check for unaligned access for both pSrc and pDst during memory copy.

--- src/comps/mcuxClMemory/src/mcuxClMemory.c
+++ src/comps/mcuxClMemory/src/mcuxClMemory.c
@@ -35,7 +35,9 @@ MCUX_CSSL_FP_PROTECTED_TYPE(void) mcuxClMemory_copy (uint8_t *pDst, uint8_t cons
     MCUX_CSSL_FP_FUNCTION_ENTRY(mcuxClMemory_copy);
 
     MCUX_CSSL_ANALYSIS_COVERITY_START_FALSE_POSITIVE(INTEGER_OVERFLOW, "modular arithmetic, mod 4")
-    uint32_t unalignedBytes = (0u - (uint32_t)pDst) % (sizeof(uint32_t));
+    uint32_t unalignedBytesDst = (0u - (uint32_t)pDst) % (sizeof(uint32_t));
+    uint32_t unalignedBytesSrc = (0u - (uint32_t)pSrc) % (sizeof(uint32_t));
+    uint32_t unalignedBytes = (unalignedBytesDst == unalignedBytesSrc) ? unalignedBytesDst : length;
     MCUX_CSSL_ANALYSIS_COVERITY_STOP_FALSE_POSITIVE(INTEGER_OVERFLOW)
     MCUX_CSSL_FP_LOOP_DECL(mcuxClMemory_copy_loop);
 
@@ -76,8 +78,7 @@ MCUX_CSSL_FP_PROTECTED_TYPE(void) mcuxClMemory_copy (uint8_t *pDst, uint8_t cons
     for (; ((i + sizeof(uint32_t)) <= length) && ((i + sizeof(uint32_t)) <= bufLength); i += sizeof(uint32_t))
     {
         MCUX_CSSL_ANALYSIS_COVERITY_START_FALSE_POSITIVE(INTEGER_OVERFLOW, "p32Dst will be in the valid range pDst[0 ~ bufLength] and pSrc will be in the valid range pSrc[0 ~ length].")
-        uint32_t crtWordVal = ((uint32_t)*(pSrc + 3) << 24) | ((uint32_t)*(pSrc + 2) << 16);
-        crtWordVal = crtWordVal | ((uint32_t)*(pSrc + 1) << 8) | (uint32_t)*pSrc;
+        uint32_t crtWordVal = ((uint32_t)*(pSrc + 3) << 24) | ((uint32_t)*(pSrc + 2) << 16) | ((uint32_t)*(pSrc + 1) << 8) | (uint32_t)*pSrc;
         MCUX_CSSL_FP_LOOP_ITERATION(mcuxClMemory_copy_loop);
         *p32Dst = crtWordVal;
         MCUX_CSSL_FP_LOOP_ITERATION(mcuxClMemory_copy_loop);
