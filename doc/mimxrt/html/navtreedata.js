5645c4c4d53a7ef1b058a8d783f6783adf58a88b
Vit Solcik (nxf07484) <vit.solcik@nxp.com>
2024-07-08 14:47:07 +0200

    [MCUX-70090][RT700] Update clib v1.10 - doc

---
+++ doc/mimxrt/html/navtreedata.js
@@ -0,0 +1,70 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "MCUX CLNS", "index.html", [
+    [ "User Manual of Crypto Library Normal Secure (CLNS)", "index.html", "index" ],
+    [ "User Guidance Manual", "a01685.html", null ],
+    [ "Security and Integration Guidance Manual", "a01686.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"a00002.html",
+"a00365_source.html",
+"a00716.html#abd7006ddfedf0234b7aa67e42978f08a",
+"a00992.html",
+"a01019.html",
+"a01067.html#gae0e48994f0652e42fec3acca8c4469bb",
+"a01098.html",
+"a01147.html#ga4d5a98a0869d936e6c471b2b66a689ec",
+"a01211.html#ga9c6252af0f1985853ed349c5d1ae1788",
+"a01256.html#ga0a9aad95e1f13908964da62ada5fd81d",
+"a01315.html#gaea55b60e770930e47ba8954ac548b7bb",
+"a01585.html#a0c56a02caf693cda953b333b89a51389"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
