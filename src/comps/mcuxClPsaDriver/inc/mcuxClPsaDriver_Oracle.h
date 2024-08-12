ed45d29007aba60f316d10ce0fdd8e5059d424b7
Ruchika Gupta <ruchika.gupta_1@nxp.com>
2024-08-12 13:16:07 +0530

    [MCUX-70570] Modify key generate wrapper in PSA driver
    Following changes have been made
    - Add interface to Free Slot in Oracle.
    - Add interface to Unload Key after storing the key (in Key Generation)
    - For key generation in ELS50 Key slot, don't use the existimng
    implementation of Key Gen Command.
    KeyGen command generates a ECC key pair where only ECC private key is
    exported out as RFC3394 Blob. This can't be imported as a ECC Key Pair
    after reset or during Load of Key. Hence this functionality is not
    compatible with PSA API's.
    
    Instead the shared secret generated via KeyExchange is used a seed
    for this random key generation of AES/ECC key. The function has been
    added in Oracle till we find the correct file to keep it.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta_1@nxp.com>

--- src/comps/mcuxClPsaDriver/inc/mcuxClPsaDriver_Oracle.h
+++ src/comps/mcuxClPsaDriver/inc/mcuxClPsaDriver_Oracle.h
@@ -241,6 +241,36 @@ psa_status_t mcuxClPsaDriver_Oracle_GetBuiltinKeyBuffer(psa_key_attributes_t *at
                                                        size_t *key_buffer_length);
 MCUX_CSSL_ANALYSIS_STOP_PATTERN_DESCRIPTIVE_IDENTIFIER()
 
+/**
+    @brief Oracle function for freeing storage for a key that will be created by the psa driver
+
+    This function requests to free storage space from the Oracle for a key that was earlier reserved.
+    This may be needed in case there is failure in a subsequenet operation in key generation and space
+    reserved by oracle for a key needs to be freed to prevent memory leakage.
+
+    @retval PSA_SUCCESS                 The operation was succesful
+    @retval PSA_ERROR_NOT_SUPPORTED     The Oracle shall never return this error code
+    @retval PSA_ERROR_GENERIC_ERROR     The operation failed (other error codes can be used as well if more specific)
+
+*/
+psa_status_t mcuxClPsaDriver_Oracle_FreeKey( mcuxClKey_Descriptor_t   *pKey );
+
+/**
+    @brief Oracle function for generating a s50 key. 
+
+    This function generates a random S50 Key using Key exchange.
+    Ideally this function should be part of PSA driver.
+
+    @retval PSA_SUCCESS                 The operation was succesful
+    @retval PSA_ERROR_NOT_SUPPORTED     The Oracle shall never return this error code
+    @retval PSA_ERROR_GENERIC_ERROR     The operation failed (other error codes can be used as well if more specific)
+
+*/
+psa_status_t mcuxClPsaDriver_Oracle_generate_s50_random_key(
+    const psa_key_attributes_t *attributes,
+    mcuxClEls_KeyIndex_t *key_index_private_key);
+
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
