d81f967080ddfeaf22a75d910e07cd50762fd458
Vit Solcik (nxf07484) <vit.solcik@nxp.com>
2025-06-06 13:35:23 +0200

    Revert "[MCUX-79464][ELS_PKC] Temporary workaround GCC 14.2 build issue in ASM"
    This reverts commit f4ff4f8a5106c1478892874914a62e0f8bf096df.

--- src/comps/mcuxClMath/inc/internal/mcuxClMath_Internal_SecModExp.h
+++ src/comps/mcuxClMath/inc/internal/mcuxClMath_Internal_SecModExp.h
@@ -73,7 +73,7 @@
  ********************************************************************************
  * It swaps ofsAt and ofsA(1-t) (stored in ofsAs), and outputs ofsYX = ofsA(1-t) || ofsMi.
  */
-#if defined(ICCARM_ARMCLANG_GNUC) && 0 // Temporary woraround!!!
+#if defined(ICCARM_ARMCLANG_GNUC)
 
 #define MCUXCLMATH_SECMODEXP_WRITEOFFSET(ofsY_ofsX,maskVal) MCUXCLPKC_PS1_SETXY_REG(ofsY_ofsX)
 #define MCUXCLMATH_SECMODEXP_SECUREOFFSETSELECT(expW0_, expW1_, ofsAs_, ofsYX_, rndW_, bIdx_, oMsH8_, oMsL8_, rnd64_0_, rnd64_1_, mask_)  \
@@ -117,8 +117,7 @@ do{  \
 } while (false)
 
 #else
-// Temporary woraround!!!
-//#warning Unsupported compiler. The above section must be manually adapted to support the inline assembly syntax.
+#warning Unsupported compiler. The above section must be manually adapted to support the inline assembly syntax.
 #define MCUXCLMATH_SECMODEXP_WRITEOFFSET(ofsY_ofsX,maskVal) MCUXCLPKC_PS1_SETXY_REG(ofsY_ofsX)
 #define MCUXCLMATH_ROR(x,y) (((x) >> (y)) | ((x) << ((32u - (y)) & 31u)))
 #define MCUXCLMATH_SECMODEXP_SECUREOFFSETSELECT(expW0, expW1, ofsA, ofsYX, rndW, bIdx, oMsH8, oMsL8,rand64_0,rand64_1,mask)  \
